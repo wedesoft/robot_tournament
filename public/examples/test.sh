1c781acb6e6794021543b859d8b7e35cf29cf406
Jan Wedekind <jan@wedesoft.de>
2011-11-14 20:28:27 +0000

    Changed syntax

--- public/examples/test.sh
+++ public/examples/test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ruby tc_wedesoft.rb && rm -f wedesoft.zip && rm -f wedesoft/.move.un~ && zip -9r wedesoft.zip ./wedesoft && curl http://10.1.50.119:3000/players --data-binary @wedesoft.zip && rm wedesoft.zip
+ruby1.8 tc_wedesoft.rb && ruby1.9 tc_wedesoft.rb && rm -f wedesoft.zip && rm -f wedesoft/.move.un~ && zip -9r wedesoft.zip ./wedesoft && curl http://10.1.50.119:3000/players --data-binary @wedesoft.zip && rm wedesoft.zip
